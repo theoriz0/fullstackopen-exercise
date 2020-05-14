0c4c5994563c529bf314dc818c0b439fe79d7a11
zhang00710 <zhang_00710@sina.com>
2020-05-15 06:31:59 +0800

    finish ex1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+
+const Counter = (props) => {
+  return <p>{props.name} {props.count}</p>
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <Button onClick={addGood} text="good" />
+        <Button onClick={addNeutral} text="neutral" />
+        <Button onClick={addBad} text="bad" />
+      </div>
+      <h1>statistics</h1>
+      <div>
+        <Counter name="good" count={good} />
+        <Counter name="neutral" count={neutral} />
+        <Counter name="bad" count={bad} />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
