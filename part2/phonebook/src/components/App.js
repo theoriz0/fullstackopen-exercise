53796503ffc19f4a6f4660427929e21f8dfa2961
zhang00710 <zhang_00710@sina.com>
2020-05-16 16:00:50 +0800

    finish ex2.7

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -13,8 +13,12 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName}))
-    setNewName('')
+    if (persons.find(i => i.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+    }
   }
 
   return (
