ecbe2246213aa6fd3d2733d30150e9a3efed141c
zhang00710 <zhang_00710@sina.com>
2020-05-27 23:38:11 +0800

    finish ex3.15 3.16 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,57 +1,54 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const Person = require('./model/person')
+const Person = require("./model/person");
 
 const app = express();
-morgan.token('body', (req) => {
+
+app.use(express.static("build"));
+app.use(express.json());
+
+morgan.token("body", (req) => {
   console.log(req.body);
   return JSON.stringify(req.body);
 });
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(express.static('build'))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then(result => {
-    res.send(result)
-  })
+  Person.find({}).then((result) => {
+    res.send(result);
+  });
 });
 
 app.get("/info", (req, res) => {
-  Person.find({}).then(result => {
+  Person.find({}).then((result) => {
     res.send(
       `<p>Phonebook has info for ${result.length} people</p><p>${Date()}</p>`
-    )
-  })
+    );
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then(result => {
-    if (result) {
-      res.send(result)
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(400).send('malformmated id')
-  })
+  Person.findById(id)
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
+    .then((result) => {
+      res.status(204).end();
     })
-    .catch(error => {
-      console.log(error)
-      res.status(400).json({
-        error: error.message
-      })
-    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -62,7 +59,7 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  if (Person.find({'name': body.name})) {
+  if (Person.find({ name: body.name })) {
     return res.status(400).json({
       error: "name must be unique",
     });
@@ -70,13 +67,30 @@ app.post("/api/persons", (req, res) => {
     const person = new Person({
       name: body.name,
       number: body.number,
-    })
+    });
 
-    person.save().then(savedPerson => {
-      res.json(savedPerson)
-    })
+    person.save().then((savedPerson) => {
+      res.json(savedPerson);
+    });
   }
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
