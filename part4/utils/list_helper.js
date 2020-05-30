cfcd3b56313f3cbca4b1b4fe57f54b402cd5a949
zhang00710 <zhang_00710@sina.com>
2020-05-31 00:56:04 +0800

    finish ex4.5 ex4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,35 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, blog) => {
+    if (!favorite) {
+      return blog
+    }
+    return (favorite.likes > blog.likes) ? favorite : blog
+  }
+  return blogs.reduce(reducer, undefined)
+}
+
+const mostBlogs = (blogs) => {
+  const counter = (countByArthur, blog) => {
+    (countByArthur[blog.author] === undefined) ? countByArthur[blog.author] = 1 : countByArthur[blog.author] ++
+    return countByArthur
+  }
+  const counts = blogs.reduce(counter, {})
+  console.log(counts)
+  let arthorThatWriteMost = {name: null, blogs: 0}
+  for (let author in counts) {
+    if (counts[author] > arthorThatWriteMost.blogs) {
+      arthorThatWriteMost = { name: author, blogs: counts[author] }
+    }
+  }
+  return arthorThatWriteMost
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
