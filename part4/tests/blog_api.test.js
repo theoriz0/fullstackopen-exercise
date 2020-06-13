0b9aff1428e890a398f4221da869ee4d06b9d34d
zhang00710 <zhang_00710@sina.com>
2020-06-13 14:49:04 +0800

    finish ex4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -11,6 +11,11 @@ test('correct amount of blogs are returned', async () => {
   expect(response.body).toHaveLength(1)
 })
 
+test('the unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.map(b => expect(b.id).toBeDefined())
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
