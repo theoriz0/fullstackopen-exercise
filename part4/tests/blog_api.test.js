fa0fe5643655a93c58e2b0517b8ebe8b22d97026
zhang00710 <zhang_00710@sina.com>
2020-06-13 14:42:40 +0800

    finish ex4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+jest.setTimeout(20000)
+
+test('correct amount of blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(1)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
