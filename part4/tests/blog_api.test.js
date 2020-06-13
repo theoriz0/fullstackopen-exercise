87ece4f0b854e117e62aadd265e8d51f18af6fbf
zhang00710 <zhang_00710@sina.com>
2020-06-13 15:32:30 +0800

    finish ex4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,19 +1,68 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
 jest.setTimeout(20000)
 
+const initialBlogs = [
+  {
+    title: 'Test title',
+    author: 'Daniel',
+    url: 'http://localhost:3001',
+    likes: 1,
+  },
+  {
+    title: 'Test title2',
+    author: 'Daniel2',
+    url: 'http://localhost:3002',
+    likes: 2,
+  }
+]
+
+const newBlog = {
+  title: 'Test title3',
+  author: 'Daniel3',
+  url: 'http://localhost:3003',
+  likes: 3,
+}
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
 test('correct amount of blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(1)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 test('the unique identifier is named id', async () => {
   const response = await api.get('/api/blogs')
   response.body.map(b => expect(b.id).toBeDefined())
+})
+
+test('post to /api/blogs creates new blog', async () => {
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const authors = response.body.map(blog => blog.author)
+  const likes = response.body.map(blog => blog.likes)
+  const titles = response.body.map(blog => blog.title)
+  const urls = response.body.map(blog => blog.url)
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(authors).toContain(newBlog.author)
+  expect(likes).toContain(newBlog.likes)
+  expect(urls).toContain(newBlog.url)
+  expect(titles).toContain(newBlog.title)
 })
 
 afterAll(() => {
